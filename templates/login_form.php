341ac641c5daad49889ef986efd174e7103cd27a
makiomar <prosentra@gmail.com>
2019-08-24 13:02:28 +0300

    escape translations

--- templates/login_form.php
+++ templates/login_form.php
@@ -3,21 +3,29 @@
         <?php _e( 'Check your email for a link to reset your password.', 'personalize-login' ); ?>
     </p>
 <?php endif; ?>
+
 <?php if ( $attributes['password_updated'] ) : ?>
     <p class="login-info">
         <?php _e( 'Your password has been changed. You can sign in now.', 'personalize-login' ); ?>
     </p>
 <?php endif; ?>
+
  <div class="grid-col widgeted">
 	 <div id="usercontrol" class="user-control">
-			<p class="hint">Enter your credintals</p>
+			<p class="hint"><?php esc_html_e('Enter your credintals', 'usercontrol') ?></p>
+
 			<form id="login" class="user-control-form" method="post" action="<?php echo wp_login_url(); ?>">
-			  <input type="text" name="log" id="user_login" class="username"  placeholder="<?php _e('&#xf090; Email Adress','user-control');?>"/>
-			  <input type="password" name="pwd" id="user_pass"  class="password"  placeholder="<?php _e('&#xf09c; Password','user-control');?>"/>
+
+			  <input type="text" name="log" id="user_login" class="username"  placeholder="<?php esc_html_e('&#xf090; Email Adress','user-control');?>"/>
+
+			  <input type="password" name="pwd" id="user_pass"  class="password"  placeholder="<?php esc_html_e('&#xf09c; Password','user-control');?>"/>
+
 			  <p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"/> <?php esc_html_e( 'Remember Me' ); ?></label></p>
-			  <button type="submit" class="smpg-button" form="login"><?php _e('Login','user-control');?></button>
-			  <a class="forgot-password" href="<?php echo wp_registration_url(); ?>"><?php _e( 'Register', 'user-control' ); ?></a>
-			  <a class="forgot-password" href="<?php echo wp_lostpassword_url(); ?>"><?php _e( 'Forgot your password?', 'user-control' ); ?></a>
+
+			  <button type="submit" class="smpg-button" form="login"><?php esc_html_e('Login','user-control');?></button>
+			  
+			  <a class="forgot-password" href="<?php echo wp_registration_url(); ?>"><?php esc_html_e( 'Register', 'user-control' ); ?></a>
+			  <a class="forgot-password" href="<?php echo wp_lostpassword_url(); ?>"><?php esc_html_e( 'Forgot your password?', 'user-control' ); ?></a>
 			  
 			</form>	
 	</div>
