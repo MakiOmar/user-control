38ad6a4559b9e4a876e07aba6cc86509f76ad849
makiomar <prosentra@gmail.com>
2019-08-24 13:07:59 +0300

    Add deleted by mistake method

--- classes/user-control.php
+++ classes/user-control.php
@@ -94,6 +94,32 @@ class ANONY__User_Control {
     }
 
     /**
+     * Redirect to password reset
+     */
+    public function redirect_password_reset() {
+	    if ( 'GET' == $_SERVER['REQUEST_METHOD'] ) {
+	        // Verify key / login combo
+	        $user = check_password_reset_key( $_REQUEST['key'], $_REQUEST['login'] );
+
+	        if ( ! $user || is_wp_error( $user ) ) {
+	            if ( $user && $user->get_error_code() === 'expired_key' ) {
+	                wp_redirect( home_url( ANONY_LOGIN.'?login=expiredkey' ) );
+	            } else {
+	                wp_redirect( home_url( ANONY_LOGIN.'?login=invalidkey' ) );
+	            }
+	            exit;
+	        }
+	 
+	        $redirect_url = home_url( ANONY_LOST );
+	        $redirect_url = add_query_arg( 'login', esc_attr( $_REQUEST['login'] ), $redirect_url );
+	        $redirect_url = add_query_arg( 'key', esc_attr( $_REQUEST['key'] ), $redirect_url );
+	 
+	        wp_redirect( $redirect_url );
+	        exit;
+	    }
+	}
+
+    /**
      * Manage form actions
      * @return type
      */
