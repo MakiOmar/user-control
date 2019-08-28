cbe091cbd25e673b1fa4459cd5a076c015543ff6
makiomar <prosentra@gmail.com>
2019-08-28 11:55:25 +0300

    Use walker to render the links insted of manual render

---
+++ classes/ucntrlogItems.php
@@ -0,0 +1,17 @@
+<?php
+if( !defined( 'ABSPATH' ) )
+	die( 'What are you trying to do?' );
+	
+class ANONY__ucntrlogItems {
+	public $db_id = 0;
+	public $object = 'ucntrlog';
+	public $object_id;
+	public $menu_item_parent = 0;
+	public $type = 'custom';
+	public $title;
+	public $url;
+	public $target = '';
+	public $attr_title = '';
+	public $classes = array();
+	public $xfn = '';
+}
