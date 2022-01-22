4e8cd86c6c0e466e8620ca166a3b446511558708
unknown <mo7amed.maki@gmail.com>
2022-01-22 13:49:54 +0200

    Change message location

--- templates/login_form.php
+++ templates/login_form.php
@@ -3,27 +3,33 @@ if( !defined( 'ABSPATH' ) )
 	die( 'What are you trying to do?' );
 ?>
 
-<?php if ( $attributes['lost_password_sent'] ) : ?>
-    <p class="login-info">
-        <?php esc_html_e( 'A reset password link has been sent to your email.', ANONY_UC_TEXTDOM ); ?>
-    </p>
-<?php endif; ?>
-
-<?php if ( $attributes['password_updated'] ) : ?>
-    <p class="login-info">
-        <?php _e( 'Your password has been changed. You can sign in now.', ANONY_UC_TEXTDOM ); ?>
-    </p>
-<?php endif; ?>
-
  <div class="grid-col widgeted">
+    <?php if ( $attributes['lost_password_sent'] ) : ?>
+        <p class="login-info">
+            <?php esc_html_e( 'A reset password link has been sent to your email.', ANONY_UC_TEXTDOM ); ?>
+        </p>
+    <?php endif; ?>
+    
+    <?php if ( $attributes['password_updated'] ) : ?>
+        <p class="login-info">
+            <?php esc_html_e( 'Your password has been changed. You can sign in now.', ANONY_UC_TEXTDOM ); ?>
+        </p>
+    <?php endif; ?>
+    
+    <?php if ( isset($_GET['registered']) && $_GET['registered'] == '1' ) : ?>
+        <p class="login-info">
+            <?php esc_html_e( 'Thank you!. Your account credentials has been emailed to the registered e-mail', ANONY_UC_TEXTDOM ); ?>
+        </p>
+    <?php endif; ?>
+    
 	 <div id="usercontrol" class="user-control">
 			<p class="hint"><?php esc_html_e('Enter your credintals', ANONY_UC_TEXTDOM) ?></p>
 
-			<form id="login" class="user-control-form" method="post" action="<?php echo wp_login_url(); ?>">
+			<form id="login" class="user-control-form" method="post" action="<?php echo wp_login_url(); ?>" autocomplete="on">
 
-			  <input type="text" name="log" id="user_login" class="username"  placeholder="<?php esc_html_e('&#xf090; Email Adress','user-control');?>"/>
+			  <input type="text" name="log" id="user_login" class="username"  placeholder="<?php esc_html_e('Email Adress','user-control');?>"/>
 
-			  <input type="password" name="pwd" id="user_pass"  class="password"  placeholder="<?php esc_html_e('&#xf09c; Password','user-control');?>"/>
+			  <input type="password" name="pwd" id="user_pass"  class="password"  placeholder="<?php esc_html_e('Password','user-control');?>"/>
 
 			  <p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"/> <?php esc_html_e( 'Remember Me' ); ?></label></p>
 
